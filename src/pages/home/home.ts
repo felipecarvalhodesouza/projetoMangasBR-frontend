f58306a1fdc66a9a8b4e9a2adc3fa19e1b9bf55c
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-05-02 21:02:50 -0300

    Correção de erro 500 no logout e método de espera para evitar erro de requisição assíncrona ao buscar coleção do usuário

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { CollectionPage } from '../collection/collection'
 import { CredenciaisDTO } from '../../models/credenciais.dto';
 import { AuthService } from '../../services/auth.service';
 import { CollectionService } from '../../services/collection.service';
+import { StorageService } from '../../services/storage.service';
 
 @IonicPage()
 @Component({
@@ -20,7 +21,8 @@ export class HomePage {
   constructor(public navCtrl: NavController,
               public menu: MenuController,
               public auth: AuthService,
-              public collectionService: CollectionService) {
+              public collectionService: CollectionService,
+              public storageService: StorageService) {
 
   }
 
@@ -45,15 +47,28 @@ export class HomePage {
     console.log(this.creds);
     this.menu.swipeEnable(true);
     this.collectionService.findUser();
+    this.waitSeconds(500);
   }
 
   ionViewDidEnter(){
-    this.auth.refreshToken()
-    .subscribe(response => {
-      this.auth.successfulLogin(response.headers.get('Authorization'));
-      this.navCtrl.setRoot('CollectionPage');
-    },
-    error => {});
+    if(this.storageService.getLocalUser()!=null){
+      this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CollectionPage');
+      },
+      error => {});
+    }
+  }
+
+  waitSeconds(iMilliSeconds) {
+    var counter= 0
+        , start = new Date().getTime()
+        , end = 0;
+    while (counter < iMilliSeconds) {
+        end = new Date().getTime();
+        counter = end - start;
+    }
   }
 
 }
